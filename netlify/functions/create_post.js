0fb8ccf2bdff3f7ce16364d3ee79e8d5c8874c18
Andrew Milne <Andrew.milne@kellogg.northwestern.edu>
2021-05-26 10:33:03 -0500

    Backend Session 1

---
+++ netlify/functions/create_post.js
@@ -0,0 +1,53 @@
+//Goal: Provide a function to create a new post in Firebase
+
+// Allows us to use firebase
+let firebase = require('./firebase')
+
+// To add: What to do if some of the fields are left blank? Pass through with blanks or return error message? 
+
+//For this to work, I need to be passed: title, brand, category, condition, delivery, dimension_h, dimension_l, dimension_w, photo, price, and description from form and usename/userId from info all from index.js
+
+// Pass through this -> /.netlify/functions/create_post?userName=Brian&userId=_______&imageUrl=____&title=____&category=_____.....
+exports.handler = async function(event) {
+
+  // get the querystring parameters and store in memory
+  let userName = event.queryStringParameters.userName
+  let userId = event.queryStringParameters.userId
+  let title = event.queryStringParameters.title
+  let category = event.queryStringParameters.category
+  let brand = event.queryStringParameters.brand
+  let condition = event.queryStringParameters.condition
+  let delivery = event.queryStringParameters.delivery
+  let dimension_h = event.queryStringParameters.dimension_h
+  let dimension_l = event.queryStringParameters.dimension_l
+  let dimension_w = event.queryStringParameters.dimension_w
+  let imageUrl = event.queryStringParameters.imageUrl
+  let price = event.queryStringParameters.price
+  let description = event.queryStringParameters.desription
+
+  // establish a connection to Firebase in memory
+  let db = firebase.firestore()
+
+  // create a new post, wait for it to return
+  // Note: cleared out posts from KelloggGram content. Could also use "posts_final" to avoid confusion
+  await db.collection(`posts`).add({
+    userName: userName,
+    userId: userId,
+    title: title,
+    category: category,
+    condition: condition,
+    delivery: delivery,
+    dim_h: dimension_h,
+    dim_l: dimension_l,
+    dim_w: dimension_w,
+    imageUrl: imageUrl,
+    price: price,
+    description: description,
+    brand: brand
+  }) 
+
+  let returnValue = [] // sample only...
+  return {
+    statusCode: 200,
+  }
+}
---
+++ netlify/functions/posts.js
@@ -0,0 +1,58 @@
+// Goal: Pull all posts from Firebase and return them as an object to index.js
+
+// To add: Allow to pass through search criteria and vary pull from Firebase based on it or cover with conditional in the loop (may be easier to use include)
+//         Could also allow users to filter by different categories, ascending/descending and pass that through in the postQuery
+
+// allows us to use firebase
+let firebase = require('./firebase')
+
+// /.netlify/functions/posts
+exports.handler = async function(event) {
+
+  // define an empty Array to hold the return value from lambda
+  let returnValue = []
+
+  // establish a connection to firebase in memory
+  let db = firebase.firestore()
+
+  // perform a query against firestore for all posts, wait for it to return, store in memory
+  let postsQuery = await db.collection(`posts`.orderBy(`created`,`desc`).get())
+
+  // retrieve the documents from the query
+  let posts = postsQuery.docs
+
+  //loop through the post documents
+  for (let i=0;i<posts.length;i++)
+
+    // get the ID from the doc
+    let postId = posts[i].id
+
+    // get the data from the doc
+    let postData = posts[i].data()
+
+    // create an Object to be added to the return value
+    let postObject = {
+      userName: userName,
+      userId: userId,
+      title: title,
+      category: category,
+      brand: brand,
+      condition: condition,
+      delivery: delivery,
+      dim_h: dim_h,
+      dim_l: dim_l,
+      dim_w: dim_w,
+      imageUrl: imageUrl,
+      price: price,
+      description: description
+    }
+    // add the Object to the return value
+    returnValue.push(postObject)
+
+  // Return the value of our lambda
+  let returnValue = [] 
+  return {
+    statusCode: 200,
+    body: JSON.stringify(returnValue)
+  }
+}
