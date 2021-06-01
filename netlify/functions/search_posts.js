d3aca8ae42ca8b91321cb2746f1044d47a3d7de6
Andrew Milne <Andrew.milne@kellogg.northwestern.edu>
2021-06-01 15:19:29 -0500

    Backend 5

--- netlify/functions/search_posts.js
+++ netlify/functions/search_posts.js
@@ -1,29 +1,30 @@
 // Goal: Return select posts from Firebase based on search criterion/criteria passed through
+// Note: This uses two different searches: exact match for category and includes for condition. 
+
 
 // allows us to use firebase
 let firebase = require('./firebase')
 
-// /.netlify/functions/search_posts?searchTitle=______&searchCategory=_______
+// /.netlify/functions/search_posts?searchCategory=______&searchCondition=_______
 exports.handler = async function(event) {
 
 // get the querystring parameters and store in memory
   let searchCondition = event.queryStringParameters.searchCondition
   let searchCategory = event.queryStringParameters.searchCategory
-
+console.log(searchCategory)
   // define an empty Array to hold the return value from lambda
   let returnValue = []
 
   // establish a connection to firebase in memory
   let db = firebase.firestore()
 
-  // perform a query against firestore for all posts, wait for it to return, store in memory
-  let postsQuery = await db.collection(`posts`.orderBy(`created`,`desc`).get())
+  // perform a query against firestore for all posts matching the criteria passed through, wait for it to return, store in memory
+  let postsQuery = await db.collection(`posts`).where(`category`,`==`,searchCategory).get()
 
-  // retrieve the documents from the query
   let posts = postsQuery.docs
 
   //loop through the post documents
-  for (let i=0;i<posts.length;i++)
+  for (let i=0;i<posts.length;i++){
 
     // get the ID from the doc
     let postId = posts[i].id
@@ -31,34 +32,30 @@ exports.handler = async function(event) {
     // get the data from the doc
     let postData = posts[i].data()
 
-    if ( //Conditional to check if a post matches the search category and condition or just the search condition if the category search is blank or just the category if the condition search is blank
-        (postData.condition.includes(searchCondition) && postData.category.includes(searchCategory)) || 
-        ((searchCondition = undefined || searchCondition.length == 0) && postData.category.includes(searchCategory))
-        ((searchCategory = undefined || searchCategory.length == 0) && postData.condition.includes(searchCondition))
-        ) {
-        // If the post matches, create an object and populate it with the relevant fields
-        postObject = {
-            userName: userName,
-            userId: userId,
-            category: category,
-            brand: brand,
-            condition: condition,
-            delivery: delivery,
-            imageUrl: imageUrl,
-            price: price,
-            description: description
+    if(postData.condition.includes(searchCondition)){
+    
+    postObject = {
+        id: postId,  
+        userName: postData.userName,
+        userId: postData.userId,
+        category: postData.category,
+        brand: postData.brand,
+        condition: postData.condition,
+        delivery: postData.delivery,
+        imageUrl: postData.imageUrl,
+        price: postData.price,
+        description: postData.description
           }
         // add the Object to the return value
     returnValue.push(postObject)
-
-      }
-
+        }
+    }
   // Return the value of our lambda
-
-
-  let returnValue = [] // sample only...
+  
   return {
     statusCode: 200,
     body: JSON.stringify(returnValue)
   }
+
+
 }
